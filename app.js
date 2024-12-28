303b03ae0094397ced352341d9e8284d2dd5e307
Athena Nefos <athena.nefos@gmail.com>
2024-12-27 22:38:36 -0500

    decalration of robin

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
 //Simple Adventurer with basic Properties
 
- const adventurer = {
-    name: "Robin",
-    health: 10,
-    inventory: ["sword", "potion", "artifact"],
-    companion: {
-        name: "Leo",
-        type: "Cat",
-        companion: {
-            name: "Frank",
-            type: "Flea",
-            inventory: ["small hat", "sunglasses"]
-        }
-    },
+// const adventurer = {
+//    name: "Robin",
+//    health: 10,
+//    inventory: ["sword", "potion", "artifact"],
+//    companion: {
+//        name: "Leo",
+//        type: "Cat",
+//        companion: {
+//            name: "Frank",
+//            type: "Flea",
+//            inventory: ["small hat", "sunglasses"]
+//        }
+//    },
 
 //method for dice roll.  mod is a modifying number, can be used in the call
 
-roll (mod = 0) {
-    const result = 
-    Math.floor(Math.random() * 20) + 1 + mod;
-
-    console.log(`${this.name} rolled a ${result}.`);
-}
-}
-adventurer.roll();
-adventurer.roll();
-adventurer.roll();
+// roll (mod = 0) {// 
+//    const result = 
+//    Math.floor(Math.random() * 20) + 1 + mod;
+//
+//    console.log(`${this.name} rolled a ${result}.`);
+// }
+// }
+// adventurer.roll();
+// adventurer.roll();
+// adventurer.roll();
 
 //Accessing items in the inventory using dot notation and square bracket syntax
-console.log("Welcome, " + adventurer.name + "!");
+// console.log("Welcome, " + adventurer.name + "!");
 
 // Access the items in the inventory using the index counter and square brackets, in this case [0, 1, 2]
 
-console.log("Your first item is: " + adventurer.inventory[0]);
+// console.log("Your first item is: " + adventurer.inventory[0]);
 
-console.log("Your second item is: " + adventurer.inventory[1]);
+// console.log("Your second item is: " + adventurer.inventory[1]);
 
-console.log("Your third item is: " + adventurer.inventory[2]);
+// console.log("Your third item is: " + adventurer.inventory[2]);
 
 // For Loop to loop through all the inventory items
 
-console.log("\nHere is a list of your inventory:");
-for (let i = 0; i < adventurer.inventory.length; i++) {
-    console.log(`${i + 1}. ${adventurer.inventory[i]}`);
-}
+// console.log("\nHere is a list of your inventory:");
+// for (let i = 0; i < adventurer.inventory.length; i++) {
+//     console.log(`${i + 1}. ${adventurer.inventory[i]}`);
+// }
 
 
 
@@ -64,19 +64,19 @@ class Character {
 
 }
 
-const robin = new Character("Robin");
-robin.inventory = ["sword", "potion", "artifact"];
+// const robin = new Character("Robin");
+// robin.inventory = ["sword", "potion", "artifact"];
 
-robin.companion = new Character("Leo");
-robin.companion.type = "Cat";
+// robin.companion = new Character("Leo");
+// robin.companion.type = "Cat";
 
-robin.companion.companion = new Character("Frank");
-robin.companion.companion.type = "Flea";
-robin.companion.companion.inventory = ["small hat", "sunglasses"];
+// robin.companion.companion = new Character("Frank");
+// robin.companion.companion.type = "Flea";
+// robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
-robin.roll();
-robin.companion.roll();
-robin.companion.companion.roll();
+// robin.roll();
+// robin.companion.roll();
+// robin.companion.companion.roll();
 
 class Adventurer extends Character {
     constructor (name, role, skill) {
@@ -147,3 +147,33 @@ frank.useAbility();
 console.log(ariana);
 console.log(ariana.companion);
 console.log(ariana.companion.companion);
+
+// new declaration of Robin and companions
+const robin = new Adventurer ("Robin", "Warrior", "Swordsmanship");
+
+//Robin's inventory
+robin.inventory.push("sword", "potion", "artifact");
+
+//create Robin's companions
+const leoge = new Companion("Leoge", "Cat", "Keen Senses");
+const frankie = new Companion("Frankie", "Flea", "Tiny Escape");
+
+//build companion hierarchy
+robin.companion = leoge;
+leo.companion = frank;
+
+// Test functnionality
+console.log(robin);
+robin.scout();
+robin.displaySkill();
+
+leo.useAbility();
+leo.decreaseLoyalty(30);
+leo.increaseLoyalty(20);
+
+frank.useAbility();
+
+//View the companion structure 
+console.log(robin.companion);
+console.log(robin.companion.companion);
+
